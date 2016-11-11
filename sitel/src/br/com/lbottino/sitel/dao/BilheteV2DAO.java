41e6e68548407ec9743f79e7830d85004311d964
luis <luis@eti31044>
2016-11-11 13:06:14 -0200

    criação da classe bilhete e serviço

---
+++ sitel/src/br/com/lbottino/sitel/dao/BilheteV2DAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.lbottino.sitel.dao;
+
+import br.com.lbottino.sitel.model.BilheteV2;
+
+/**
+ *
+ * @author luis
+ */
+public class BilheteV2DAO extends GenericDAO<BilheteV2>{
+
+    public BilheteV2DAO() {
+        super(BilheteV2.class);
+    }
+    
+}
---
+++ sitel/src/br/com/lbottino/sitel/dao/ServicoV2DAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.lbottino.sitel.dao;
+
+import br.com.lbottino.sitel.model.ServicoV2;
+
+/**
+ *
+ * @author luis
+ */
+public class ServicoV2DAO extends GenericDAO<ServicoV2>{
+
+    public ServicoV2DAO() {
+        super(ServicoV2.class);
+    }
+    
+}
